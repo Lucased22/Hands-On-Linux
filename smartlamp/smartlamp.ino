7623bb7c115ffa7bcee449bdf558af17b051ba19
Lucas França <lucasfrranca@gmail.com>
2025-06-04 20:54:16 -0400

    feat: Creating lead manipulation

---
+++ smartlamp/smartlamp.ino
@@ -0,0 +1,65 @@
+// Pinos usados
+int ledPin = 18;     // GPIO do LED
+int ldrPin = 34;     // GPIO analógico do LDR
+
+int ldrMax = 4000;   // Valor máximo estimado do LDR (ajuste conforme seu teste)
+int led_intensity = 10;  // Valor inicial do LED
+
+void setup() {
+  Serial.begin(9600);
+
+  // Inicializa PWM no canal 0
+  ledcSetup(0, 5000, 8);         // Canal 0, 5kHz, resolução 8 bits (0–255)
+  ledcAttachPin(ledPin, 0);      // Liga o pino ao canal
+
+  pinMode(ldrPin, INPUT);
+
+  // Inicializa o LED com brilho 10%
+  ledUpdate(led_intensity);
+
+  Serial.println("SmartLamp Initialized.");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    processCommand(command);
+  }
+}
+
+// Processa os comandos recebidos via serial
+void processCommand(String command) {
+  if (command.startsWith("SET_LED ")) {
+    int value = command.substring(8).toInt();
+    if (value >= 0 && value <= 100) {
+      ledUpdate(value);
+      Serial.println("OK");
+    } else {
+      Serial.println("IGNORED");
+    }
+
+  } else if (command == "GET_LED") {
+    Serial.println(led_intensity);
+
+  } else if (command == "GET_LDR") {
+
+  } else {
+    Serial.println("UNKNOWN_COMMAND");
+  }
+}
+
+// Atualiza intensidade do LED via PWM
+void ledUpdate(int value) {
+  led_intensity = value;
+  int pwmValue = map(value, 0, 100, 0, 255); // Converte de 0–100 para 0–255
+  ledcWrite(0, pwmValue);
+}
+
+// Lê e normaliza o valor do LDR (0 a 100)
+//int ldrGetValue() {
+ // int ldrRaw = analogRead(ldrPin);
+  //ldrRaw = constrain(ldrRaw, 0, ldrMax);   // Garante que não ultrapasse ldrMax
+ // int ldrNormalized = map(ldrRaw, 0, ldrMax, 0, 100); // Converte para 0–100
+  //return ldrNormalized;
+//}
